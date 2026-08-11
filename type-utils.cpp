cb6c935ec1dbb137b64250d2ee5ecfedfd508675
Troy-Davies <164232406+troy-davies@users.noreply.github.com>
2026-08-11 19:19:15 +0100

    Update type-utils.cpp to use std::map to make std::cout work
    No idea if this works.

--- type-utils.cpp
+++ type-utils.cpp
@@ -7,6 +7,7 @@
 using std::string;
 using std::vector;
 using std::stringstream;
+using std::map;
 
 vector<string> typeutils::splitString(string input) {
 	/* 	Splits a string into a list of "words", separates by spaces 
