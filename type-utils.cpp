df5145be0d00e934677adb807335151c35f3dc1e
Troy-Davies <164232406+troy-davies@users.noreply.github.com>
2026-08-11 18:55:35 +0100

    Update type-utils.cpp to allow not crash upon receiving unexpected characters such as whitespace
    Unless it receives newline or any known command, the application will now just display an invalid command message. At the moment, typing "ver" should crash as I plan to add that as a command in the future.

--- type-utils.cpp
+++ type-utils.cpp
@@ -12,6 +12,7 @@ vector<string> typeutils::splitString(string input) {
 	/* 	Splits a string into a list of "words", separates by spaces 
 		Requires a string as an argument only */
 	if (input == "") {return {""};}
+	else if (input != "q" && input != "n" && input != "r" && input != "p" && input != "t" && input != "s" && input != "d" && input != "\n" && input != "ver") {std::cout << "That is not a valid input! type \"help\" for help \n";}
 	stringstream ss(input); // using stringstream type as it is a simple way to separate words due to the type's methods
 	vector<string> split = {};
 	string word;
