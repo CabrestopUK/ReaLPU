a0b17fd2feba22665c9e743720754f99ee79d50e
Troy <164232406+troy-davies@users.noreply.github.com>
2026-08-17 10:17:16 +0100

    Update salt.c to generate windows random string and start work on linux
    This is the part that makes the salt for future use in encryption of the database.

--- salt.c
+++ salt.c
@@ -1,7 +1,15 @@
 #include <stddef.h>
 #include <stdint.h>
-#if defined(_WIN32) // If compiling for windows
+
+#if defined(_WIN32)
 #include <windows.h>
 #include <bcrypt.h>
 
-#pragma comment(lib, "bcrypt.lib")
+int get_random_bytes(void *buf, size_t len) {
+    return BCryptGenRandom(NULL, (PUCHAR)buf, (ULONG)len,
+                           BCRYPT_USE_SYSTEM_PREFERRED_RNG) == 0;
+}
+
+#elif defined(__linux__)
+#include <fcntl.h>
+#include <unistd.h>
